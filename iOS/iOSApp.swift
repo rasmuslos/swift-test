15c6dbad4d89b95c1bd57399a811fa746b015945
Rasmus Krämer <git@rfk.io>
2022-05-24 13:50:29 +0200

    Added asset contents

---
+++ iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Rasmus Krämer on 12.03.22.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
